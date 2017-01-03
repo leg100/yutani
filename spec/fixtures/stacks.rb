b4e7a443d79c11e984f1bbe2d05530f2391ddb60
Louis Garman <louisgarman@gmail.com>
2017-01-03 15:25:18 +0000

    Test use of scope yield

--- spec/fixtures/stacks.rb
+++ spec/fixtures/stacks.rb
@@ -1,5 +1,5 @@
-scope(env: 'dev', component: 'vpc') do
-  stack('dev', 'eu-west-1', 'vpc') do
+scope(env: "dev", component: "vpc") do |s|
+  stack(s[:env], :'eu-west-1', :vpc) do
     inc { 'vpc.rb' }
   end
 end
